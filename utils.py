5caf8cdce2799e91f4f05dd6df7dab8630f75ed7
Giacomo Salici <giambo2000@gmail.com>
2025-10-01 14:46:09 +0200

    plot

--- utils.py
+++ utils.py
@@ -46,23 +46,20 @@ def displayTS(ts: np.ndarray, raw_ts: np.ndarray = None, save_path: str = None):
     if num_variates == 1:
         axes = [axes]  
 
-
     for i, ax in enumerate(axes):
         # Plot processed ts
-        
-
         if raw_ts is not None:
-            ax.plot(raw_ts[:, i], label=f'Raw TS', color = "#1f77b4", linewidth=3)
-
-        
-        ax.plot(ts[:, i], label=f'TS', color = "#ff357c", linewidth=1)
-         # Plot raw ts if provided
-        
-        ax.set_title(f'Variate {i+1}', fontsize=14, weight="bold")
+            ax.plot(raw_ts[:, i], label=f'Raw TS', color="#1f77b4", linewidth=3)
+        ax.plot(ts[:, i], label=f'TS', color="#ff357c", linewidth=1)
+        ax.set_title(f'Variate {i+1}', fontsize=14)
         ax.set_ylabel('Value', fontsize=12)
-        ax.legend(frameon=True, loc="best")
 
     axes[-1].set_xlabel('Time Steps', fontsize=12)
+
+    # Add a single legend at the bottom
+    handles, labels = axes[-1].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower right', ncol=2, frameon=False, fontsize=10)
+    fig.subplots_adjust(bottom=0.15)  # Adjust layout to make space for the legend
 
     if save_path:
         plt.savefig(save_path, dpi=300, bbox_inches="tight")
