bf4a99cd26aaf4c46a17de9724ac1964a7f89de9
Giacomo Salici <giambo2000@gmail.com>
2025-10-16 22:02:06 +0200

    refatored normal generator

---
+++ generators/normal.py
@@ -0,0 +1,40 @@
+import numpy as np
+from .base import BaseGenerator
+from typing import Literal
+
+
+class NormalGenerator(BaseGenerator):
+    def __init__(
+        self,
+        shape: tuple[int] = None,
+        ts: np.ndarray | None = None,
+        combine_domain: Literal["time", "frequency"] = None,
+        combine_mode: Literal["add", "mul"] | None = None,
+        mean=0.0,
+        std=0.1,
+    ):
+        """
+        Initialize the NormalNoiseGenerator.
+
+        Args:
+            shape: (tuple[int]): the shape of the TS to generate
+            ts: (np.ndarray): time series to "clone" as of shape and other stuff, not clone in the literal sense.
+            combine_domain (str): Domain in which the combine operation happens when `apply` method is called ('time' or 'frequency').
+            combine_mode (str): Mode in which the combine operation happens when `apply` method is called ('add' or 'mul').
+            mean : float, default=0.0
+                Mean of the Gaussian noise to be added.
+            std : float, default=0.1
+                Standard deviation of the Gaussian noise to be added.
+        """
+        super().__init__(shape, ts, combine_domain, combine_mode)
+        self.mean = mean
+        self.std = std
+
+    def generate(self) -> np.ndarray:
+
+        noise = np.random.normal(self.mean, self.std, self.shape)
+
+        if self.combine_mode == "add":
+            noise = noise + self.get_base_ts()
+
+        return noise
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from utils import displayTS, setSeed
 
 
-from generators.normalNoise import NormalNoiseGenerator
+from generators.normal import NormalGenerator
 from generators.costant import CostantGenerator
 from generators.sinusoid import SinusoidGenerator
 from generators.mask import MaskGenerator
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     
     
     anomalies = [
-        NormalNoiseGenerator(mean=0, std=0.1, domain="time"),
+        NormalGenerator(mean=0, std=0.1, domain="time"),
         CostantGenerator(anomaly_fraction=0.01, anomaly_value=1.0, anomaly_length=5, anomaly_length_variance=2, domain="time"),
-        NormalNoiseGenerator(mean=0, std=0.1, domain="frequency"),
+        NormalGenerator(mean=0, std=0.1, domain="frequency"),
     ]
     
     masks = MaskGenerator(shape) 
