effedace9e8a6e1d5d4406bc12d33b3ec2d2aafc
Giacomo Salici <giambo2000@gmail.com>
2025-10-16 20:58:53 +0200

    way better base [BREAK ALL]

--- generators/base.py
+++ generators/base.py
@@ -4,60 +4,78 @@ import numpy as np
 
 
 class BaseGenerator(ABC):
-    def __init__(self, domain: Literal['time', 'frequency'] = 'time'):
+    def __init__(self, shape: tuple[int] = None, 
+                 ts: np.ndarray | None = None, 
+                 combine_domain: Literal['time', 'frequency'] = None, 
+                 combine_mode: Literal['add', 'mul'] | None = None):
         """
         Initialize the BaseGenerator.
 
         Args:
-            domain (str): Domain in which the generator operates ('time' or 'frequency').
+            shape: (tuple[int]): the shape of the TS to generate
+            ts: (np.ndarray): time series to "clone" as of shape and other stuff, not clone in the literal sense.
+            combine_domain (str): Domain in which the combine operation happens when `apply` method is called ('time' or 'frequency').
+            combine_mode (str): Mode in which the combine operation happens when `apply` method is called ('add' or 'mul').
+
         """
-        if domain not in ['time', 'frequency']:
+        
+        assert shape != None or ts != None, "shape and ts can't be both None"
+        
+        if shape is None:
+            shape = ts.shape
+            
+        self.shape = shape
+        self.seq_len, self.no_variates = self.shape
+        
+        
+        if combine_domain and combine_domain not in ['time', 'frequency']:
             raise ValueError("Domain must be either 'time' or 'frequency'")
-        self.domain = domain
+        
+        self.combine_domain = combine_domain
+        
+        if combine_mode and combine_mode not in ['add', 'mul']:
+            raise ValueError("Combine Mode not allowed")
+        
+        self.combine_mode = combine_mode
     
     @abstractmethod
-    def generate(self, ts: np.ndarray, **params) -> np.ndarray:
-        pass
-    
-    def apply(self, ts: np.ndarray, params: Dict = {}) -> np.ndarray:
-        """
-        Apply the generator to the input time series in the specified domain.
-
-        Args:
-            ts (np.ndarray): Input time series data of shape (seq_len, no_variates).
-            params (Dict): Additional parameters for the generate method.
-        """
+    def generate(self) -> np.ndarray:
+        raise NotImplementedError("Can't generate with base generator.")
         
-        if self.domain == 'frequency':
+    def combine(self, ts: np.ndarray, generated_ts: np.ndarray):
+        assert ts.shape == generated_ts.shape
+        
+        if self.combine_domain == 'frequency':
             ts = np.fft.fft(ts, axis=0)
+            
+        assert self.combine_mode and self.combine_domain
         
-        ts = self.generate(ts, params)
+        match self.combine_mode:
+            case "add":
+                ts = ts + generated_ts
+
+            case "mul":
+                ts = ts * generated_ts
         
-        if self.domain == 'frequency':
+        if self.combine_mode == 'frequency':
             ts = np.fft.ifft(ts, axis=0).real
         
-        return ts
-
-
-class AdditiveGenerator(BaseGenerator):
-    def __init__(self, shape: tuple[int], domain: Literal['time', 'frequency'] = 'time'):
-        """
-        Initialize the AdditiveGenerator.
-        
-        Args:
-            shape (tuple[int]): Shape of the base matrix to which components will be added.
-            domain (str): Domain in which the generator operates ('time' or 'frequency').
-        """
-        super().__init__(domain)
-        self.base = np.zeros(shape)
+        return ts    
     
-    @abstractmethod
-    def generate(self, **params) -> np.ndarray:
+    
+    def generate_and_combine(self, ts: np.ndarray) -> np.ndarray:
         """
-        Generate a component to be added to the base matrix.
+        Generate the TS and combine it to the input time series in the specified domain.
+
+        Args:
+            ts (np.ndarray): Input time series data.
+        """
         
-        Returns:
-            np.ndarray: Component to add to the base matrix.
-        """
-        pass
+        generated_ts = self.generate()
+        return self.combine(ts, generated_ts)
+      
+
+    def apply(**kwargs):
+        print("Warning, this method is deprecated and will be removed soon.")
+
     
