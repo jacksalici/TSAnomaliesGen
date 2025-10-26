04a7cbfc5c024c12b018ea83fb348888ebbd8af5
Giacomo Salici <giambo2000@gmail.com>
2025-10-25 23:03:07 -0700

    better example

--- main.py
+++ main.py
@@ -10,35 +10,64 @@ from generators.mask import MaskGenerator
 from generators.drift import DriftGenerator
 
 if __name__ == "__main__":
-    #setSeed(21)
-    
+    setSeed(21)
+
     shape = (1000, 4)
-        
-    
-    raw_ts = np.zeros(shape)
-   
-    
+
+    raw_ts = SinusoidGenerator(
+        shape, amplitude=0.5, phase=np.pi, max_frequency=5
+    ).generate()
+
     some = Some(
         [
             Maybe(
-                NormalGenerator(shape, mean=0, std=0.01, combine_domain="frequency", combine_mode="add"),
-                MaskGenerator(shape, intra_variates_probability=1, inter_variates_probability=1),
-                probability=1
+                NormalGenerator(
+                    shape, mean=0, std=0.01, combine_domain="time", combine_mode="add"
+                ),
+                MaskGenerator(
+                    shape, intra_variates_probability=1, inter_variates_probability=1
+                ),
+                probability=1,
             ),
             Maybe(
-                SinusoidGenerator(shape, amplitude=0.5, phase=np.pi, max_frequency=5, combine_domain = "time", combine_mode="add"),
-                MaskGenerator(shape, intra_variates_probability=0.2, inter_variates_probability=0.35),
-                probability=0.5
+                NormalGenerator(
+                    shape, mean=0, std=0.2, combine_domain="time", combine_mode="add"
+                ),
+                MaskGenerator(
+                    shape, intra_variates_probability=0.2, inter_variates_probability=1
+                ),
+                probability=0.5,
             ),
             Maybe(
-                DriftGenerator(shape, drift_type="exponential", combine_domain = "time", combine_mode="add"),
-                MaskGenerator(shape, intra_variates_probability=1, inter_variates_probability=1),
-                probability=1
+                SinusoidGenerator(
+                    shape,
+                    amplitude=0.1,
+                    phase=2 * np.pi,
+                    max_frequency=5,
+                    combine_domain="time",
+                    combine_mode="add",
+                ),
+                MaskGenerator(
+                    shape, intra_variates_probability=1, inter_variates_probability=1
+                ),
+                probability=1,
             ),
-            
+            Maybe(
+                DriftGenerator(
+                    shape,
+                    drift_type="exponential",
+                    drift_rate_range=(0.05, 1),
+                    combine_domain="time",
+                    combine_mode="add",
+                ),
+                MaskGenerator(
+                    shape, intra_variates_probability=1, inter_variates_probability=1
+                ),
+                probability=1,
+            ),
         ]
     )
-    
-    ts = some.generate_and_combine(raw_ts) 
+
+    ts = some.generate_and_combine(raw_ts)
 
     displayTS(ts, raw_ts, save_path="dummy_time_series.png")
